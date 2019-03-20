5b658e1b38d98a8425a2bee83ee9187b3e0e1b63
wanting <luvianmoon@163.com>
2019-03-20 16:51:32 +0000

    check

--- ConcentrationLight.py
+++ ConcentrationLight.py
@@ -55,21 +55,21 @@ def main():
                     # Use single channel audio to detect voice activity
                     if vad.is_speech(chunk[0::CHANNELS].tobytes(), RATE):
                         speech_count += 1
-                        sys.stdout.write('1')
-                        if countratenumber == 150:
-                            if lights[1].brightness <= 200:
-                                lights[0].brightness -= 10
-                                lights[1].brightness += 10
-                                countratenumber = 0
-                                if lights[1].saturation <= 254:
-                                    lights[1].saturation += 50
+                        # sys.stdout.write('1')
+                        # if countratenumber == 150:
+                        #     if lights[1].brightness <= 200:
+                        #         # lights[0].brightness -= 10
+                        #         # lights[1].brightness += 10
+                        #         # countratenumber = 0
+                        #         if lights[1].saturation <= 254:
+                        #             lights[1].saturation += 50
                     else:
                         sys.stdout.write('0')
                         if countratenumber == 150:
                             if lights[0].brightness <= 200:
-                                lights[0].brightness += 10
-                                lights[1].brightness -= 10
-                                countratenumber = 0
+                                # lights[0].brightness += 10
+                                # lights[1].brightness -= 10
+                                # countratenumber = 0
                                 if lights[1].saturation >= 20:
                                     lights[1].saturation -= 10
 
