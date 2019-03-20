1c21bfd2059b65ca1e0217a4aebaf16e2433c005
wanting <luvianmoon@163.com>
2019-03-20 11:54:26 +0000

    test

--- ConcentrationLight.py
+++ ConcentrationLight.py
@@ -29,16 +29,17 @@ def get_response_from_ip(b):
 
 #================================================================================
 def main():
+    print("hello/n")
     """
     b = Bridge('192.168.1.64')
     get_response_from_ip(b)
     lights = b.lights
     lights[0].brightness = 200
-    
+
     for light in lights:
         light
-    
-    
+
+
     lights[1].brightness = 0
     lights[0].hue = 33858
     lights[1].hue = 33858
