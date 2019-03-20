96eb9308ab099af8f4240b3763fa3fd15d8fa3b9
wanting <luvianmoon@163.com>
2019-03-20 12:11:34 +0000

    change

--- ConcentrationLight.py
+++ ConcentrationLight.py
@@ -30,21 +30,21 @@ def get_response_from_ip(b):
 #================================================================================
 def main():
 
-    # b = Bridge('192.168.1.64')
-    # get_response_from_ip(b)
-    # lights = b.lights
-    # lights[0].brightness = 200
-    #
-    # for light in lights:
-    #     light
-    #
-    #
-    # lights[1].brightness = 0
-    # lights[0].hue = 33858
-    # lights[1].hue = 33858
-    # lights[1].hue = 65057
-    # lights[0].saturation = 44
-    # lights[1].saturation = 20
+    b = Bridge('192.168.1.64')
+    get_response_from_ip(b)
+    lights = b.lights
+    lights[0].brightness = 200
+
+    for light in lights:
+        light
+
+
+    lights[1].brightness = 0
+    lights[0].hue = 33858
+    lights[1].hue = 33858
+    lights[1].hue = 65057
+    lights[0].saturation = 44
+    lights[1].saturation = 20
 
 
     while True:
@@ -60,12 +60,12 @@ def main():
                     if vad.is_speech(chunk[0::CHANNELS].tobytes(), RATE):
                         speech_count += 1
                         sys.stdout.write('1')
-                        #if lights[1].brightness <= 200:
-                         #   lights[0].brightness -= 5
-                          #  lights[1].brightness += 10
-                           # lights[1].hue += 5
-                            #if lights[1].saturation <= 254
-                             #   lights[1].hue -= 5
+                        if lights[1].brightness <= 200:
+                           lights[0].brightness -= 5
+                           lights[1].brightness += 10
+                           lights[1].hue += 5
+                            # if lights[1].saturation <= 254
+                            #    lights[1].hue -= 5
 
 
                     else:
