8cdff8efed29d178a68037b3d36c5ab15a82484d
wanting <luvianmoon@163.com>
2019-03-20 16:39:57 +0000

    .

--- ConcentrationLight.py
+++ ConcentrationLight.py
@@ -58,17 +58,16 @@ def main():
                         # sys.stdout.write('1')
                         if countratenumber == 150:
                             if lights[1].brightness <= 200:
-                                lights[0].brightness -= 100
-                                lights[1].brightness += 100
+                                lights[0].brightness -= 10
+                                lights[1].brightness += 10
                                 if lights[1].saturation <= 254:
-                                    print (lights[1].saturation)
                                     lights[1].saturation += 100
                     else:
                         # sys.stdout.write('0')
                         if countratenumber == 150:
                             if lights[0].brightness <= 200:
-                                lights[0].brightness += 100
-                                lights[1].brightness -= 100
+                                lights[0].brightness += 10
+                                lights[1].brightness -= 10
                                 countratenumber = 0
                                 if lights[1].saturation >= 20:
                                     lights[1].saturation -= 10
