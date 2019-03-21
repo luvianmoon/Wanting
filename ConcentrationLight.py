7d552bd43389edacebb3e14040f181207bac642d
wanting <luvianmoon@163.com>
2019-03-21 16:24:41 +0000

    loop connect bridge

--- ConcentrationLight.py
+++ ConcentrationLight.py
@@ -32,74 +32,74 @@ def get_response_from_ip(b):
 
 #================================================================================
 def main():
-    b = Bridge('192.168.1.64')
-    try:
-        get_response_from_ip(b)
-    except phue.PhueRequestTimeout as e:
-        time.sleep(3)
-        os.execv(sys.executable, ['ConcentrationLight.py'] + sys.argv)
-        exit()
-    lights = b.lights
-    lights[0].brightness = 200
-    lights[1].brightness = 0
-    lights[0].hue = 33858
-    lights[1].hue = 65057
-    lights[0].saturation = 44
-    lights[1].saturation = 10
-    countratenumber = 0
-
     while True:
-        vad = webrtcvad.Vad(3)
-        speech_count = 0
-        chunks = []
-        doa_chunks = int(DOA_FRAMES / VAD_FRAMES)
-
+        b = Bridge('192.168.1.64')
         try:
-            with MicArray(RATE, CHANNELS, RATE * VAD_FRAMES / 1000)  as mic:
-                for chunk in mic.read_chunks():
-                    countratenumber += 1
-                    if vad.is_speech(chunk[0::CHANNELS].tobytes(), RATE):
-                        speech_count += 1
-                        sys.stdout.write('1')
-                        if countratenumber > 1000:
-                            if lights[1].brightness <= 240:
-                                lights[0].brightness -= 40
-                                lights[1].brightness += 40
-                                countratenumber = 0
-                                # print ('lightis' + str(lights[1].saturation))
-                                if lights[1].saturation <= 254:
-                                    lights[1].saturation += 30
+            get_response_from_ip(b)
+        except phue.PhueRequestTimeout:
+            time.sleep(3)
+            continue
+        lights = b.lights
+        lights[0].brightness = 200
+        lights[1].brightness = 0
+        lights[0].hue = 33858
+        lights[1].hue = 65057
+        lights[0].saturation = 44
+        lights[1].saturation = 10
+        countratenumber = 0
+
+        while True:
+            vad = webrtcvad.Vad(3)
+            speech_count = 0
+            chunks = []
+            doa_chunks = int(DOA_FRAMES / VAD_FRAMES)
+
+            try:
+                with MicArray(RATE, CHANNELS, RATE * VAD_FRAMES / 1000)  as mic:
+                    for chunk in mic.read_chunks():
+                        countratenumber += 1
+                        if vad.is_speech(chunk[0::CHANNELS].tobytes(), RATE):
+                            speech_count += 1
+                            sys.stdout.write('1')
+                            if countratenumber > 1000:
+                                if lights[1].brightness <= 240:
+                                    lights[0].brightness -= 40
+                                    lights[1].brightness += 40
+                                    countratenumber = 0
+                                    # print ('lightis' + str(lights[1].saturation))
+                                    if lights[1].saturation <= 254:
+                                        lights[1].saturation += 30
 
 
-                    else:
-                        sys.stdout.write('0')
-                        if countratenumber > 1000:
-                            if lights[0].brightness <= 200:
-                                lights[0].brightness += 30
-                                lights[1].brightness -= 30
-                                countratenumber = 0
-                                # print ('lightis' + str(lights[1].saturation))
-                                if lights[1].saturation >= 20:
-                                    lights[1].saturation -= 30
+                        else:
+                            sys.stdout.write('0')
+                            if countratenumber > 1000:
+                                if lights[0].brightness <= 200:
+                                    lights[0].brightness += 30
+                                    lights[1].brightness -= 30
+                                    countratenumber = 0
+                                    # print ('lightis' + str(lights[1].saturation))
+                                    if lights[1].saturation >= 20:
+                                        lights[1].saturation -= 30
 
 
-                    sys.stdout.flush()
-                    chunks.append(chunk)
-                    if len(chunks) == doa_chunks:
-                        if speech_count > (doa_chunks / 2):
-                            pass
+                        sys.stdout.flush()
+                        chunks.append(chunk)
+                        if len(chunks) == doa_chunks:
+                            if speech_count > (doa_chunks / 2):
+                                pass
 
-                        speech_count = 0
-                        chunks = []
+                            speech_count = 0
+                            chunks = []
 
-        except KeyboardInterrupt:
-            pass
+            except KeyboardInterrupt:
+                pass
 
-        # except PhueRequestTimeout:
-        #     time.sleep(3)
-        #     os.execv(sys.executable, ['ConcentrationLight.py'] + sys.argv)
+            # except PhueRequestTimeout:
+            #     time.sleep(3)
+            #     os.execv(sys.executable, ['ConcentrationLight.py'] + sys.argv)
 
-    pixel_ring.off()
+        pixel_ring.off()
 
 #================================================================================
 if __name__ == '__main__':
