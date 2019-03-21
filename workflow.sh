55067637635e6958866091757d008f6cf1c3bfe3
wanting <luvianmoon@163.com>
2019-03-21 11:54:56 +0000

    。

--- workflow.sh
+++ workflow.sh
@@ -9,3 +9,13 @@
 # On the Pi
 cd ~/Wanting
 git pull
+
+
+# After finished the coding:
+# write code to let raspberry pi run script after lxde(the interface that appear when electricity cable was pug in)
+
+# open up the file using nano:
+sudo nano /etc/xdg/lxsession/LXDE/autostart
+# add the following line to the bottom of the file (just as last line)
+@sh /home/pi/myproject/myscript.sh"别人的文件，可以用py文件代替”
+# save and reboot!
