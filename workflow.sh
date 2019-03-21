019975ecbc2c437f948d6c6f4f3ecbfc0ae47442
wanting <luvianmoon@163.com>
2019-03-21 18:40:52 +0000

    record the steps

--- workflow.sh
+++ workflow.sh
@@ -11,14 +11,26 @@ cd ~/Wanting
 git pull
 
 
-# After finished the coding:
-# write code to let raspberry pi run script after lxde(the interface that appear when electricity cable was pug in)
+# Step to do after finished the coding:
+# write code to let raspberry pi run script after turn on(the interface that appear when electricity cable was pug in)
+
+# create Shell to put python file in
+# https://www.instructables.com/id/Raspberry-Pi-Launch-Python-script-on-startup/
+nano launcher.sh
+sh /home/pi/Wanting/launcher.sh
+
+# Set WiFi network priority
+https://raspberrypi.stackexchange.com/questions/58304/how-to-set-wifi-network-priority
+
+
 
 # open up the file using nano: call
+# https://learn.sparkfun.com/tutorials/how-to-run-a-raspberry-pi-program-on-startup#method-2-autostart
 nano /home/pi/.config/autostart/clock.desktop
 # write
 [Desktop Entry]
 Type=Application
 Name=Clock
 Exec=x-terminal-emulator -e python2 /home/pi/Wanting/ConcentrationLight.py
+# above code ask pi to open terminal and run
 # save & exit
